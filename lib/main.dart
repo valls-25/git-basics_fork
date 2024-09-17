c154c22ced47c3d55f63377c069b4ae27fa0edc3
Manan Gandhi <ardumanan@gmail.com>
2024-09-17 15:13:18 +0530

    Text change

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Hello World!"),
+          title: const Text("Hello World! This is a test"),
         ),
       ),
     ),
